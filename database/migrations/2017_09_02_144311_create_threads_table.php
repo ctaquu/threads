bda6b5eb406d753f10dca265ab2dc5d08f95062a
Veljkovic Damjan <ctaquu@Damjans-MacBook-Pro.local>
2017-09-02 16:53:09 +0200

    feature(migrations): add migrations for threads and comments tables

---
+++ database/migrations/2017_09_02_144311_create_threads_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('desc');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('threads', function (Blueprint $table) {
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('threads', function (Blueprint $table) {
+            $table->dropForeign('threads_user_id_foreign');
+        });
+
+        Schema::drop('threads');
+    }
+}
